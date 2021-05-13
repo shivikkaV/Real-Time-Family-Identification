2dc5120867d5b7e24437f505d956d7992fcc0853
shivikkaV <74470668+shivikkaV@users.noreply.github.com>
2021-05-13 16:04:48 -0400

    Add files via upload

---
+++ main.js
@@ -0,0 +1,15 @@
+window.alert("♥♥ Did you have a good day so far? ♥♥")
+
+function preload(){
+    
+}
+
+function setup(){
+    canvas = createCanvas(300, 300);
+    canvas.center();
+}
+
+function draw(){
+    
+}
+
